07031c0396dded7729743a46ec8b21b22170a3d2
pranjalsh22 <physics0403@gmail.com>
2025-11-26 00:40:57 +0530

    Update darkmatterwebapp.py

--- darkmatterwebapp.py
+++ darkmatterwebapp.py
@@ -44,7 +44,7 @@ def plot_it(r,v,error,x_label,y_label,title,logscale=False):
 # Streamlit app layout
 st.title("Study of Dark Matter")
 st.header("galaxy rotation curve")
-st.write("By Pranjal Sharma")
+st.write("Developed by Pranjal Sharma")
 st.write("Under the guidance of Dr. C. Konar")
 st.sidebar.write("Upload a text file with galaxy rotaton curve data")
 
